59c13d4df0efc65cadd89d5f2f83b4501437cad4
thiago henrique fragata <thiago.henrique.fragata@hotmail.com>
2021-08-05 19:34:41 -0400

    Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
+import styles from '../styles/Home.module.scss'
+
 export default function Home() {
-  return <h1>hello, world!</h1>;
+  return (
+    <>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏🏻 Hey, welcome</span>
+          <h1>News about the <span>React</span> world. </h1>
+          <p>Get access to all the publications <br/> <span>for $9.90 month</span></p>
+
+        </section>
+        <img src="./images/avatar.svg" alt="Girls coding" />
+      </main>
+    </>
+  )
 }
