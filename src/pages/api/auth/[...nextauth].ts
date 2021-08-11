8dd51e6319dfd7414b01785a1525b6a3e1b057ad
thiago henrique fragata <thiago.henrique.fragata@hotmail.com>
2021-08-10 22:07:31 -0400

    @Bug verificação de usuario duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,7 +5,6 @@ import { query as q } from "faunadb";
 import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
@@ -13,21 +12,26 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
-  jwt: {
-    signingKey: process.env.JWT_SIGNING_KEY,
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user;
 
       try {
         await fauna.query(
-          q.Create(q.Collection("users"), {
-            data: { email },
-          })
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), {
+              data: { email },
+            }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
         );
         return true;
-      } catch (error) {
+      } catch {
         return false;
       }
     },
